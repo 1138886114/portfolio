3b57887a38062d2b314b72b0827f162e33e80a55
字数统计 <1138886114@qq.com>
2019-08-15 22:13:16 +0800

    完善了Gallery APP
    设置好了Media路径和网址

--- gallery/models.py
+++ gallery/models.py
@@ -5,7 +5,9 @@ from django.db import models
 
 
 class Gallery(models.Model):
-    description = models.CharField(max_length=80)
+    description = models.CharField(default='这是作品的简介',max_length=80)
+    image = models.ImageField(default='default.png',upload_to='image/')
+    title = models.CharField(default='标题',max_length=20)
 
-    # def __str__(self):
-    #     pass
+    def __str__(self):
+        return self.title
